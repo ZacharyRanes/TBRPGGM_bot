d89cb12eba2458d13e06673872b000cb26ce7e8c
Zachary Ranes <zachary.mt.ranes@gmail.com>
2018-02-19 18:08:42 -0500

    Big Big Updates, Thanks to Sergey

--- TBRPGGM_bot.py
+++ TBRPGGM_bot.py
@@ -1,13 +1,14 @@
 #Author: Zachary Ranes
-#Written in Python 2.7, requires eternnoir/pyTelegramBotAPI to run
+#Written in Python 3, requires eternnoir/pyTelegramBotAPI to run
 
-import ConfigParser
+import configparser
 import telebot
 from telebot import types
 import requests
+import TBRPG_parser as parser
 
 #This loads a config file that holds the bots API key
-config = ConfigParser.ConfigParser()
+config = configparser.ConfigParser()
 config.read("TBRPGGM_bot_bot_config.cfg")
 token = config.get("telegram_bot_api","telegram_token")
 
@@ -17,7 +18,8 @@ bot = telebot.TeleBot(token)
 #Dictionaries 
 #key is a chat id holds an message id (message waiting to be replied to)
 waiting = {}
-
+adventures = {}
+running_adventures = {}
 
 #Help command will display the instruction file to the chat 
 @bot.message_handler(commands=['help'])
@@ -28,9 +30,9 @@ def command_help(message):
 #Gives the option to start playing a game or upload one
 @bot.message_handler(commands=['start'])
 def command_start(message):
-    bot.reply_to(message, "To start /new_adventure use this command\n"\
+    bot.reply_to(message, "To start a /new_adventure use this command\n"\
                             "To /upload_adventure use this command\n"\
-                            "For /help with with adventure formating use this command")
+                            "For /help with with adventure formatting use this command")
 
 #Prompts for a reply of an adventure file and preps for upload_reply_handler
 @bot.message_handler(commands=['upload_adventure'])
@@ -39,25 +41,30 @@ def command_upload_adventure(message):
     key = message.chat.id
     waiting[key] = reply.message_id
 
-#Helper fuction for upload_reply_handler
-def text_test(message):
-	return message.document.mime_type == 'text/plain'
-
-#
-@bot.message_handler(func=text_test, content_types=['document'])
+#Reads in files from user responce 
+@bot.message_handler(content_types=['document'])
 def upload_reply_handler(message):
     key = message.chat.id
     if key in waiting:
         if message.reply_to_message.message_id == waiting[key]:
             waiting.pop(key, None)
-            reply = bot.reply_to(message, "Reading file...")
-            file_parser(reply.message_id, message.document.file_id)
+            bot.reply_to(message, "Reading/Parsing File")
+            try:
+                file_info = bot.get_file(message.document.file_id)
+                adventure_file = bot.download_file(file_info.file_path)
+                adventures[message.document.file_name] = parser.parseAGF(adventure_file)
+                bot.reply_to(message, "Done")
+            except:
+                bot.reply_to(message, "Parsing Failed, please read /help for intustions on formating adventure files")
 
-#
-def file_parser(message_id, file_id):
-    file_info = bot.get_file(file_id)
-    adventure_file = bot.download_file(file_info.file_path)
-    #adventure_file pass this object to the parser part of the project 
+#Shows options of uploaded adventured  
+@bot.message_handler(commands=['new_adventure'])
+def command_new_adventure(message):
+    markup = types.InlineKeyboardMarkup()
+    for a in adventures:
+        markup.row(types.InlineKeyboardButton(callback_data=a, text=a))
+    bot.reply_to(message, "Which adventure do you want to play?", reply_markup=markup)
+
 
 
 bot.polling()
---
+++ TBRPG_parser.py
@@ -0,0 +1,119 @@
+import json
+import re
+from lxml import etree
+
+#to use for parsing stuff
+subst_vars = [r'(\w+)::(\w+)',r'self.states["\1"]["\2"]']
+misc_substs = [r'true',r'True']
+
+class adventureGame:
+    def __init__(self):
+        self.states  = {}    #save game-related states
+        self.data    = None  #the description of the game
+        self.pos     = None  #ptr to current place in game
+        self.choices = []    #current valid choices and their mappings to original choices
+        self.text    = ''    #current text after processing
+
+    def start(self):
+        self.pos = self.data['start_state']
+        start_node = self.data['states'][self.pos]
+        self.pruneChoices(start_node)
+        self.processText(start_node)
+        self.states = self.data['gamevars']
+
+    def choose(self, c):
+        #translate choice c to og choices
+        c = self.choices[c][0]
+        c = self.data['states'][self.pos]['options'][c]
+        #TODO: Implement random transition check&choice here
+
+        #input choice c and progress game
+        pos = c[-2] #nextNode(string)
+        nextNode = self.data['states'][pos]        #nextNode(var)
+        #get parts of the node
+
+        #==== main transition steps ====
+        self.execStmt(c[1])          # execute transition function
+        self.pruneChoices(nextNode)  # prune the choices the user can pick
+        self.processText(nextNode)   # process text based on env states
+        self.pos = pos               # finally, set our position string
+
+    def state(self):
+        #show current state, return current state text
+        return self.text
+
+    def getChoices(self):
+        return [c[-1] for c in self.choices]
+
+    def pruneChoices(self, node):
+        ch = node['options']
+        self.choices = []
+        for i in range(0,len(ch)):
+            c = ch[i]
+            if (c[0] == '' or self.evalStmt(c[0])):  #is choice valid?
+                flavorText = c[-1]
+                self.choices.append([i,flavorText])
+
+    def processText(self,node):
+        #parse string to display
+        #n = self.data['states'][node] #im now passing this directly
+        text = '<base>' + node['text'] + '</base>'
+        root = etree.fromstring(text)
+        self.text = self.parseXML(root)
+
+    def parseXML(self, root):
+        strng = ''
+        strng += root.text
+        for i in root.getchildren():  #this is where we define the tag types, could use dict for switch/case logic here
+            if (i.tag == 'cond' and self.evalStmt(i.attrib['expr'])):
+                strng += self.parseXML(i)
+            strng += i.tail
+        return strng
+
+    def isEnd(self):
+        if (len(self.data['states'][self.pos]['options']) == 0):
+            return True
+        else:
+            return False
+
+    def isWin(self):
+        return self.pos in self.data['win_states']
+
+    def execStmt(self, expr):
+        try:
+            exec(self.substPythonString(expr))
+        except:
+            return False;
+
+    def evalStmt(self, expr):
+        try:
+            return eval(self.substPythonString(expr))
+        except:
+            return 0;
+
+    def substPythonString(self, expr):
+        expr = re.sub(subst_vars[0], subst_vars[1], expr)
+        expr = re.sub(misc_substs[0], misc_substs[1], expr)
+        return expr
+
+
+def parseAGF(s):
+    #turn string s into AGF
+    ag = adventureGame()
+    ag.data = json.loads(s)
+    ag.start()
+    return ag
+
+def serialize(ag):
+    #turn adventureGame object into string
+    return json.dumps(ag.data)
+
+#load a .agf file
+def loadAGF(f):
+    with open(f) as fd:
+        ag = parseAGF(fd.read())
+    return ag
+
+def saveAGF(ag, fn):
+    with open(fn,'w') as fd:
+        fd.write(serialize(ag))
